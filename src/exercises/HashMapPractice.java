77dc2752b7c5dacdc987536c106c30e21258e93f
tvalli3 <41927664+tvalli3@users.noreply.github.com>
2020-08-19 20:15:45 -0500

    Ch. 3.7.3 complete and Ch. 3 Studio complete w/ Bonus

--- src/exercises/HashMapPractice.java
+++ src/exercises/HashMapPractice.java
@@ -7,7 +7,7 @@ import java.util.Scanner;
 public class HashMapPractice {
     public static void main(String[] args) {
 
-        HashMap<String, Double> students = new HashMap<>();
+        HashMap<Integer, String> students = new HashMap<>();
         Scanner input = new Scanner(System.in);
         String newStudent;
 
@@ -20,9 +20,9 @@ public class HashMapPractice {
             newStudent = input.nextLine();
 
             if (!newStudent.equals("")) {
-                System.out.print("Grade: ");
-                Double newGrade = input.nextDouble();
-                students.put(newStudent, newGrade);
+                System.out.print("ID: ");
+                int newID = input.nextInt();
+                students.put(newID, newStudent);
 
                 // Read in the newline before looping back
                 input.nextLine();
@@ -32,14 +32,9 @@ public class HashMapPractice {
 
         // Print class roster
         System.out.println("\nClass roster:");
-        double sum = 0.0;
 
-        for (Map.Entry<String, Double> student : students.entrySet()) {
+        for (Map.Entry<Integer, String> student : students.entrySet()) {
             System.out.println(student.getKey() + " (" + student.getValue() + ")");
-            sum += student.getValue();
         }
-
-        double avg = sum / students.size();
-        System.out.println("Average grade: " + avg);
     }
 }
---
+++ src/studios/countingcharacters/CountChars.java
@@ -0,0 +1,36 @@
+package studios.countingcharacters;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class CountChars {
+    public static void main(String[] args) {
+        String text = "If the product of two terms is zero then common sense says at least one of the two terms has " +
+                "to be zero to start with. So if you move all the terms over to one side, you can put the quadratics " +
+                "into a form that can be factored allowing that side of the equation to equal zero. Once you’ve done " +
+                "that, it’s pretty straightforward from there.";
+
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter your string here: ");
+        text = in.nextLine().toLowerCase();
+
+        char[] charArr = text.toCharArray();
+        HashMap<Character, Integer> count = new HashMap<>();
+
+        for (char letter : charArr){
+            int ascii = letter;
+            if (ascii > 96 && ascii < 123) {
+                if(count.containsKey(letter)) {
+                    count.put(letter, count.get(letter) + 1);
+                } else {
+                    count.put(letter, 1);
+                }
+            }
+        }
+
+        for(Map.Entry<Character, Integer> ch : count.entrySet()) {
+            System.out.println(ch.getKey() + " : " + ch.getValue());
+        }
+    }
+}
