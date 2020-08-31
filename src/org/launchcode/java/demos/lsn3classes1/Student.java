2af6e0f504a7b735e892e6240d66868ee544dcb0
tvalli3 <41927664+tvalli3@users.noreply.github.com>
2020-08-30 22:35:38 -0500

    Ch 4.2 Exercise Complete

--- src/org/launchcode/java/demos/lsn3classes1/Student.java
+++ src/org/launchcode/java/demos/lsn3classes1/Student.java
@@ -3,24 +3,57 @@ package org.launchcode.java.demos.lsn3classes1;
 // Start working here with your Student class.
 // To instantiate the Student class, add your code to the main in the file, SchoolPractice.
 
+import java.util.Objects;
+
 public class Student {
 
+    private static int nextStudentId = 1;
     private String name;
     private int studentId;
-    private int numberOfCredits = 0;
-    private double gpa = 0.0;
+    private int numberOfCredits;
+    private double gpa;
 
-    public Student(String name, int studentID, int numberOfCredits, double gpa) {
+    public Student(String name, int studentId,
+                   int numberOfCredits, double gpa) {
         this.name = name;
-        this.studentId = studentID;
+        this.studentId = studentId;
         this.numberOfCredits = numberOfCredits;
         this.gpa = gpa;
     }
-    public Student() {
-        this.name = "";
-        this.studentId = 0;
-        this.numberOfCredits = 0;
-        this.gpa = 0.0;
+
+    public Student(String name, int studentId) {
+        this(name, studentId, 0, 0);
+    }
+
+    public Student(String name) {
+        this(name, nextStudentId);
+        nextStudentId++;
+    }
+
+    public String studentInfo() {
+        return (this.name + " has a GPA of: " + this.gpa);
+    }
+
+    public void addGrade(int courseCredits, double grade) {
+        this.numberOfCredits += courseCredits;
+        double totalQualityScore = grade * courseCredits;
+        this.gpa = totalQualityScore / this.numberOfCredits;
+    }
+
+    public String getGradeLevel() {
+        String gradeLevel = "";
+        if (this.numberOfCredits >= 90) {
+            gradeLevel = "Senior";
+        } else if (this.numberOfCredits >= 60) {
+            gradeLevel = "Junior";
+        } else if (this.numberOfCredits >= 30) {
+            gradeLevel = "Sophomore";
+        } else if (this.numberOfCredits >= 0) {
+            gradeLevel = "Freshman";
+        } else {
+            gradeLevel = "Invalid";
+        }
+        return gradeLevel;
     }
 
     public String getName() {
@@ -53,5 +86,28 @@ public class Student {
 
     private void setGpa(double gpa) {
         this.gpa = gpa;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", studentId=" + studentId +
+                ", numberOfCredits=" + numberOfCredits +
+                ", gpa=" + gpa +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Student student = (Student) o;
+        return studentId == student.studentId;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(studentId);
     }
 }
