c75279d74207f1b51a01155ded5e38ab23a715ef
tvalli3 <41927664+tvalli3@users.noreply.github.com>
2020-09-14 01:07:25 -0500

    Added comparator interfaces for Cone and Flavor, print arraylist using generic method

--- src/org/launchcode/java/demos/lsn7interfaces/Main.java
+++ src/org/launchcode/java/demos/lsn7interfaces/Main.java
@@ -8,12 +8,19 @@ public class Main {
         ArrayList<Flavor> flavors = menu.getFlavors();
         ArrayList<Cone> cones = menu.getCones();
 
-        // TODO: Use a Comparator class to sort the 'flavors' array alphabetically by the 'name'
-        //  field.
+        flavors.sort(new FlavorComparator());
 
-        // TODO: Use a Comparator class to sort the 'cones' array in increasing order by the 'cost'
-        //  field.
+        cones.sort(new ConeComparator());
 
-        // TODO: Print the 'flavors' and 'cones' lists (in a clear manner) to verify the sorting.
+        printArray(flavors);
+        System.out.println("\n");
+        printArray(cones);
+    }
+
+    public static <T extends Ingredient> void printArray(ArrayList<T> arr){
+        for (T item : arr) {
+            Ingredient ing = (Ingredient) item;
+            System.out.println(ing.getName() + ": " + ing.getCost());
+        }
     }
 }
